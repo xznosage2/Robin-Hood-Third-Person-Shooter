b3dd427e4e159f232ee725e42789e5e5b2f52a50
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2024-01-24 20:21:35 -0700

    Completed Zombie

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -10,7 +10,10 @@ public class Enemy : MonoBehaviour
     public Transform player;
     [SerializeField] Transform attackPoint;
     [SerializeField] GameObject attackSphere;
+    [SerializeField] Transform sensePoint;
     private charaterManager manager;
+    private float speed = 0;
+    private Transform lastPosition;
 
     int wave = 1;
 
@@ -20,16 +23,22 @@ public class Enemy : MonoBehaviour
     public float timeBetweenAttacks;
     bool alreadyAttacked;
 
+    Animator anim;
+
     //states
     public float attackRange;
     public bool playerInAttackRange;
+    public bool isMoving = false;
 
 	private void Awake()
 	{
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
         manager = GetComponent<charaterManager>();
+        anim = GetComponent<Animator>();
         manager.SetHealth(wave);
+
+        lastPosition = transform;
 	}
 
 	// Start is called before the first frame update
@@ -41,28 +50,37 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(sensePoint.position, attackRange, whatIsPlayer);
 
         if (!playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange) AttackPlayer();
-    }
+        if (playerInAttackRange) anim.SetTrigger("Attack");
+
+        //update speed
+        speed = (transform.position - lastPosition.position).magnitude / Time.deltaTime;
+        lastPosition.position = transform.position;
+        anim.SetFloat("Speed", speed);
+
+	}
 
 	private void ChasePlayer()
     {
+        anim.SetBool("Moving", true);
         agent.SetDestination(player.position);
     }
 
     private void AttackPlayer()
     {
+        anim.SetBool("Moving", false);
         agent.SetDestination(transform.position);
 
         transform.LookAt(player);
 
         if(!alreadyAttacked)
         {
-            //attack code
-            Instantiate(attackSphere, attackPoint);
+			//attack code
+			Instantiate(attackSphere, attackPoint);
 
+			anim.SetTrigger("Attack");
             alreadyAttacked = true;
             Invoke(nameof(resetAttack), timeBetweenAttacks);
         }
@@ -82,5 +100,10 @@ public class Enemy : MonoBehaviour
 	{
 		Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
+	}
+
+    public void AttackSphere()
+    {
+		
 	}
 }
