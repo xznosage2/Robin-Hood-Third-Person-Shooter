858254d5198e6bee99be9ed929215185e10b349a
bfinne05 <100608199+bfinne05@users.noreply.github.com>
2024-01-25 20:12:33 -0700

    Completed Zombies Game Mode

--- Assets/Scripts/Health/charaterManager.cs
+++ Assets/Scripts/Health/charaterManager.cs
@@ -55,6 +55,7 @@ public class charaterManager : MonoBehaviour, IDamagable
     
     public void SetHealth(int wave)
     {
+        Debug.Log("Increased Health");
         health = wave * 2;
     }
 
